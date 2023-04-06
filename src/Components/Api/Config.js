264cf0ad55d956a55d1d58cdf39131cf28177ced
harshagandeti <harshavardhangandeti@gmail.com>
2023-04-06 14:54:29 +0530

    sixth commit

--- src/Components/Api/Config.js
+++ src/Components/Api/Config.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBYfynyd5KtESG_CppzCc_KwguU7x6xC4s",
-  authDomain: "sitams-electronix-club.firebaseapp.com",
-  projectId: "sitams-electronix-club",
-  storageBucket: "sitams-electronix-club.appspot.com",
-  messagingSenderId: "457970109452",
-  appId: "1:457970109452:web:deecf4637795b1fd7e3229",
-};
+// const firebaseConfig = {
+//   apiKey: "<SOME_Key>",
+//   authDomain: "<SOME_Key>",
+//   projectId: "<SOME_Key>",
+//   storageBucket: "<SOME_Key>",
+//   messagingSenderId: "<SOME_Key>",
+//   appId: "<SOME_Key>"
+// };
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
