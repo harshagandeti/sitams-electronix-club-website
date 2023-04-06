9121c8a86b2db68bba322182dd5a681da6f109a8
harshagandeti <harshavardhangandeti@gmail.com>
2023-04-06 14:55:57 +0530

    seventh commit

--- src/Components/Api/Config.js
+++ src/Components/Api/Config.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// const firebaseConfig = {
-//   apiKey: "<SOME_Key>",
-//   authDomain: "<SOME_Key>",
-//   projectId: "<SOME_Key>",
-//   storageBucket: "<SOME_Key>",
-//   messagingSenderId: "<SOME_Key>",
-//   appId: "<SOME_Key>"
-// };
+const firebaseConfig = {
+  apiKey: "<SOME_Key>",
+  authDomain: "<SOME_Key>",
+  projectId: "<SOME_Key>",
+  storageBucket: "<SOME_Key>",
+  messagingSenderId: "<SOME_Key>",
+  appId: "<SOME_Key>"
+};
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
